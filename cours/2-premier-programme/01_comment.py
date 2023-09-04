65546344676a52267592616914d8c932ccab958c
QBidolet <quentin.bidolet@gmail.com>
2023-09-04 12:16:31 +0200

    - comment.py

---
+++ cours/2-premier-programme/01_comment.py
@@ -0,0 +1,17 @@
+# Un commentaire simple.
+
+# Plusieurs lignes
+# Une autre ligne.
+
+
+"""
+Un string
+multiligne
+qui peut servir de commentaire.
+
+DOC STRING
+Décrire une fonction(a,b).
+a: description du paramètre a
+b: description de b
+Renvoie x en sortie ...
+"""
