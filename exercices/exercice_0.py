f272cbb9a15668eb7b4490b3e611db0e0d5df36f
QBidolet <quentin.bidolet@gmail.com>
2023-09-05 11:46:08 +0200

    - exercice 0

---
+++ exercices/exercice_0.py
@@ -0,0 +1,16 @@
+# Codez ici sans utilisez de nombre ni d'opérateur mathématique
+# à part le  =
+# pour obtenir le resultat que a soit égale à 6 et b égale à 5
+
+a = 5
+b = 6
+
+# Solution classique
+# c = a
+# a = b
+# b = c
+# print(a, b)
+
+# Version pythonic
+a, b = b, a
+print(a, b)
