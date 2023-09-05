c86486aca3d80bab5e2e72a55653892e224dc11a
QBidolet <quentin.bidolet@gmail.com>
2023-09-05 11:36:58 +0200

    - qcm

--- exercices/qcm.py
+++ exercices/qcm.py
@@ -1,16 +1,35 @@
-question_1 = "Quel est le ... ?"
-question_2 = "Quel est le ... ?"
-question_3 = "Quel est le ... ?"
+question_1 = "Q1. Quel est le résultat du calcul 2*2**2 ?\n" \
+             "(a)4\n" \
+             "(b)5\n" \
+             "(c)8\n" \
+             "(d)12"
+
+question_2 = "Q2. Quel est le résultat du calcul 2*2**2 ?\n" \
+             "(a)4\n" \
+             "(b)5\n" \
+             "(c)8\n" \
+             "(d)12"
+question_3 = "Q3. Un set peut il contenir une valeur dupliquée ?" \
+             "(a)Oui\n" \
+             "(b)Non\n" \
+             "(c)Peut être"
 
 questions = {
-    question_1: "a",
-    question_2: "a",
+    question_1: "c",
+    question_2: "c",
     question_3: "b",
 }
 
-ma_liste = ["a", "b"]
-saisie = input()
-while saisie not in ma_liste:
-    saisie = input()
-for question in questions:
-    print(question)
+print(" *** Bienvenue sur ce QCM. ***\n\n")
+score = 0
+for question, reponse in questions.items():
+    print(question + '\n')
+    reponse_utilisateur = input("Tapez votre réponse (a/b/c/d)")
+    if reponse_utilisateur == reponse:
+        score += 1
+        print("Bonne réponse.")
+    else:
+        print("Mauvaise réponse.")
+
+# print("Votre score : " + str(score) + "/" + str(len(questions)))
+print(f"Votre score : {score}/{len(questions)}.")
