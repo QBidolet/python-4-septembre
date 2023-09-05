858b36be441314577645bd2fdbea884a4acecff3
QBidolet <quentin.bidolet@gmail.com>
2023-09-05 13:53:09 +0200

    - exercice 2

--- exercices/exercice_2.py
+++ exercices/exercice_2.py
@@ -4,5 +4,17 @@ présent dans une liste donnée.
 Par exemple, si on l'appliquait à
 la liste [32, 5, 12, 8, 3, 75, 2, 15],
 ce programme devrait afficher : 75.
-Contraintes : Ne pas utiliser les fonctions, min, max etc.
+Contraintes : Ne pas utiliser les fonctions, min, max, sort etc.
 """
+
+ma_liste = [32, 5, 12, 8, 3, 75, 2, 15]
+
+if ma_liste:
+    max = ma_liste[0]
+    for nombre in ma_liste:
+        if nombre > max:
+            max = nombre
+    else:
+        print(f"Le maximum est {max}.")
+else:
+    print("Veuillez fournir une liste non vide.")
