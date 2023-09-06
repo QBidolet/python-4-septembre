096524ec8b9b50dd84ffcdaaa9583889cb1be41f
QBidolet <quentin.bidolet@gmail.com>
2023-09-06 16:59:30 +0200

    - exercice 5

--- exercices/exercice_5.py
+++ exercices/exercice_5.py
@@ -6,3 +6,25 @@ les prénoms, c'est-à-dire une liste avec ceux
 ayant moins de 6 caractères (6 inclus)
 et ceux ayant plus de 6 caractères.
 """
+
+# prenoms = ["Jean", "Maximilien", "Brigitte", "Sonia"]
+# prenoms_inf_6 = []
+# prenoms_sup_6 = []
+#
+# for prenom in prenoms:
+#     if len(prenom) > 6:
+#         prenoms_sup_6.append(prenom)
+#     else:
+#         prenoms_inf_6.append(prenom)
+#
+# print(prenoms_sup_6)
+# print(prenoms_inf_6)
+
+prenoms = ["Jean", "Maximilien", "Brigitte", "Sonia"]
+
+prenoms_sup_6 = [element for element in prenoms if len(element) > 6]
+prenoms_inf_6 = [element for element in prenoms if len(element) <= 6]
+
+# liste avec les 10 premiers nombres pairs au carré
+liste = [element ** 2 for element in range(0, 20) if element % 2 == 0]
+print(liste)
