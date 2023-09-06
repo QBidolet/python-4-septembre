f3dbada92a50d0561ccc6642aa207330fff2634f
QBidolet <quentin.bidolet@gmail.com>
2023-09-06 10:33:38 +0200

    - organiseur.py

--- exercices/organiseur/organiseur.py
+++ exercices/organiseur/organiseur.py
@@ -9,6 +9,8 @@
 # Exemple : dans mon dossier il y a un fichier .pdf,
 # le programme doit créer le dossier PDF
 # et déplacer le fichier à l'intérieur
+import os
+import shutil
 
 folder_dict = {
     "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", ".svg",
@@ -23,3 +25,38 @@ folder_dict = {
     "XML": [".xml"],
     "EXE": [".exe"]
 }
+
+chemin = os.path.join(os.getcwd(), "A TRIER")
+liste_fichier = os.listdir(chemin)
+for nom_fichier in liste_fichier:
+    _, extension = os.path.splitext(nom_fichier)
+    for key, values in folder_dict.items():
+        if extension in values:
+            chemin_dossier = os.path.join(os.getcwd(), key)
+            os.makedirs(chemin_dossier, exist_ok=True)
+            chemin_fichier = os.path.join(chemin, nom_fichier)
+            shutil.move(chemin_fichier, chemin_dossier)
+
+chemin_a_trier = os.path.join(os.getcwd(), "A TRIER")
+for element in os.scandir(chemin_a_trier):
+    if element.is_file():
+        _, extension = os.path.splitext(element.name)
+        # print(extension)
+        for key, values in folder_dict.items():
+            if extension in values:
+                chemin_dossier = os.path.join(os.getcwd(), key)
+                os.makedirs(chemin_dossier, exist_ok=True)
+                chemin_element = os.path.join(chemin_a_trier, element.name)
+                shutil.move(chemin_element, chemin_dossier)
+
+
+chemin_a_trier = os.path.join(os.getcwd(), "A TRIER")
+for folder, subfolders, files in os.walk(chemin_a_trier):
+    for file in files:
+        _, extension = os.path.splitext(file)
+        for key, values in folder_dict.items():
+            if extension.lower() in values:
+                chemin_dossier = os.path.join(os.getcwd(), key)
+                os.makedirs(chemin_dossier, exist_ok=True)
+                chemin_element = os.path.join(folder, file)
+                shutil.move(chemin_element, chemin_dossier)
